82a9756badbf58972a18a2da1556c3e6d093ff9b
Nikita <7468683odess@gmail.com>
2022-11-13 21:09:03 +0300

    Test/ name api

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
-
-console.log(app);
+let data = JSON.stringify({
+  data: [{
+      name: "test1"
+    },
+    {
+      name: "test2"
+    },
+  ]
+})
 
 app.get('/', (req, res) => {
-  res.send('c2ock')
+  res.send('test');
+})
+
+app.get('/api/names', (req, res) => {
+  res.send(data)
 })
 
 app.listen(PORT);
